092fde2809606e225ba02202959b8b41c83c19ae
Wanda Antoine <wanda@thinkful.com>
2020-06-27 10:26:23 -0400

    changed the URL used to request data in config.js

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 export default {
-    API_ENDPOINT: 'https://calm-bastion-83998.herokuapp.com/',
+    NOTES_ENDPOINT: 'https://calm-bastion-83998.herokuapp.com/api/notes',
+    FOLDERS_ENDPOINT: 'https://calm-bastion-83998.herokuapp.com/api/folders',
     API_KEY: process.env.REACT_APP_API_KEY,
   }
