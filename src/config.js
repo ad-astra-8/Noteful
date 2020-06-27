b205994688044635b775872613e5449938c4eb4a
Wanda Antoine <wanda@thinkful.com>
2020-06-27 09:57:34 -0400

    set up app for deploy installed vercel

--- src/config.js
+++ src/config.js
@@ -1,3 +1,4 @@
 export default {
     API_ENDPOINT: 'https://calm-bastion-83998.herokuapp.com/',
+    API_KEY: process.env.REACT_APP_API_KEY,
   }
--- src/setupTests.js
+++ src/setupTests.js
@@ -5,7 +5,8 @@
 import '@testing-library/jest-dom/extend-expect';
 
 
-import Enzyme from 'enzyme'
+  
+import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16'
 
-Enzyme.configure({ adapter: new Adapter() })
+configure({ adapter: new Adapter() })
